1b070f38485db515b2214dfe7f9732bc24c7f0f7
Štefan Jarina <stefan@jarina.cz>
2017-01-29 14:12:51 +0100

    Rework for SMF and update of README
    Huge overhaul of original fork. Completely transformed for BBCodes
    default in SMF.

--- README.md
+++ README.md
@@ -1,3 +1,214 @@
 # BBCode To Markdown Converter
 
-Live version here: https://stefanjarina.github.io/BBCode-To-Markdown-Converter/
+Currently **all tags are specific for SMF (Simple Machines Forum)**.
+Converting to *GitHub Flavored Markdown* and tested on NodeBB forum platform.
+
+Does support html output for unsupported tags
+* table, tr, th, td
+
+## Currently Supported Tags:
+
+#### Emphasis tags
+| BBCode | Markdown |
+|--------|----------|
+|[b]text[/b]|\*\*text\*\*|
+|[i]italic text[/i]|\*italic text\*|
+|[u]underlined text is replaced with cursive[/u]|\*underlined text is replaced with cursive\*|
+|[s]strikethrough text[/s]|\~\~strikethrough text\~\~|
+
+nested:
+```
+[b][i]this is italic and bold[/i][/b]
+[b]this is [s]strikethrough[/s] and this is [i]italic[/i] and all this is bold[/b]
+```
+```
+***this is italic and bold***
+**this is ~~strikethrough~~ and this is *italic* and all this is bold**
+```
+
+#### Size tags
+| BBCode | Markdown |
+|--------|----------|
+[size=24pt]test size 24pt -> h1[/size]|\# test size 24pt -> h1|
+[size=18pt]test size 18pt -> h2[/size]|\#\# test size 18pt -> h2|
+[size=14pt]test size 14pt -> h3[/size]|\#\#\# test size 14pt -> h3
+[size=12pt]test size 12pt -> h4[/size]|\#\#\#\# test size 12pt -> h4
+[size=10pt]test size 8pt -> h5[/size]|\#\#\#\#\# test size 8pt -> h5
+[size=8pt]test size 8pt -> h6[/size]|\#\#\#\#\#\# test size 8pt -> h6
+
+#### Lists
+##### Unordered list
+BBCode:
+```
+[list]
+[li]test of unordered list 1[/li]
+[li]test of unordered list 2[/li]
+[/list]
+```
+
+Markdown
+```
+* test of unordered list 1
+* test of unordered list 2
+```
+
+##### Ordered list
+BBCode:
+```
+[list type=decimal]
+[li]test of ordered list 1[/li]
+[li]test of ordered list 2[/li]
+[li]test of ordered list 3[/li]
+[li]test of ordered list 4[/li]
+[/list]
+```
+
+Markdown
+```
+1. test of ordered list 1
+1. test of ordered list 2
+1. test of ordered list 3
+1. test of ordered list 4
+```
+
+#### Code Blocks
+##### Inline Code
+BBCode:
+```
+[code]inline_code();[/code]
+```
+Markdown
+
+    `inline_code();`
+
+##### Multiline Code block with autodiscovery highlight
+
+BBCode:
+```
+[code]
+import Html exposing (text)
+
+main =
+  text "Hello, World!"
+[/code]
+```
+
+converts to:
+
+    ```
+    import Html exposing (text)
+
+    main =
+    text "Hello, World!"
+    ```
+
+##### Multiline Code block with specified language
+
+BBCode:
+```
+[code=elm]
+import Html exposing (text)
+
+main =
+  text "Hello, World!"
+[/code]
+```
+
+converts to:
+
+    ```elm
+    import Html exposing (text)
+
+    main =
+    text "Hello, World!"
+    ```
+
+#### Quotes
+##### Single line quote
+BBCode:
+```
+[quote]single line quote[/quote]
+```
+
+Markdown
+```
+> inline blockquote
+```
+
+##### Multi line quote
+BBCode:
+```
+[quote]first line
+second line
+third line
+[/quote]
+```
+
+Markdown
+```
+\> first line
+\> second line
+\> third line
+```
+
+#### Other tags
+| BBCode | Markdown |
+|--------|----------|
+|[hr]|---|
+
+### Tags supported only in HTML output
+##### Tables
+BBCode:
+```
+[table]
+[tr]
+[th]Heading 1[/th]
+[th]Heading 2[/th]
+[th]Heading 3[/th]
+[/tr]
+[tr]
+[td]first - text 1[/td]
+[td]first - text 2[/td]
+[td]first - text 3[/td]
+[/tr]
+[tr]
+[td]second - text 1[/td]
+[td]second - text 2[/td]
+[td]second - text 3[/td]
+[/tr]
+[/table]
+```
+
+Markdown
+```
+<table>
+<tr>
+<th>Heading 1</th>
+<th>Heading 2</th>
+<th>Heading 3</th>
+</tr>
+<tr>
+<td>first - text 1</td>
+<td>first - text 2</td>
+<td>first - text 3</td>
+</tr>
+<tr>
+<td>second - text 1</td>
+<td>second - text 2</td>
+<td>second - text 3</td>
+</tr>
+</table>
+```
+
+#### TODO:
+- [X] Able to turn on HTML output for special tags unsupported by markdown
+  - [X] tables (for now)
+- [ ] Support for specific BBCode for other major forums [toggleable]
+  - [ ] phpBB
+  - [ ] MyBB
+  - [ ] vBulletin
+  - [ ] FluxBB
+  - [ ] bbPress
+
+#### CREDITS:
+Originaly forked from: [JonDum](https://github.com/JonDum/BBCode-To-Markdown-Converter)
--- index.js
+++ index.js
@@ -1,39 +1,78 @@
-
 function convert() {
   var left = document.getElementById('left_ta');
   var right = document.getElementById('right_ta');
+  var html_check = document.getElementById('html_check').checked;
 
   var left_value = left.value;
 
-  //preprocessing for tf2toolbox BBCode
-  if(left_value.search(/TF2Toolbox/gmi) != -1) {
-    left_value = left_value
-    .replace(/(\(List generated at .+?\[\/URL\]\))((?:.|\n)+)/gmi, '$2\n\n\n$1') //Move TF2Toolbox link to bottom
-    .replace('(List generated at', '(List generated from')
-    .replace(/[^\S\n]+\(List/gmi,'(List')
-    .replace(/\[b\]\[u\](.+?)\[\/u\]\[\/b\]/gmi,'[b]$1[/b]\n') //Fix double emphasized titles
-    .replace(/(\n)\[\*\]\[b\](.+?)\[\/b\]/gmi, '$1\[\*\] $2');
-  }
-
-  //general BBcode conversion
+  //SMF (Simple Machines Forum) BBcode
   left_value = left_value
-    .replace(/\[b\]((?:.|\n)+?)\[\/b\]/gmi, '**$1**') //bold; replace [b] $1 [/b] with ** $1 **
-    .replace(/\[\u\]((?:.|\n)+?)\[\/\u\]/gmi, '*$1*')  //underline; replace [u] $1 [/u] with * $1 *
-    .replace(/\[s\]((?:.|\n)+?)\[\/s\]/gmi, '~~ $1~~') //strikethrough; replace [s] $1 [/s] with ~~ $1 ~~
-    .replace(/\[color\=.+?\]((?:.|\n)+?)\[\/color\]/gmi, '$1') //remove [color] tags
-    .replace(/\[list\=1\]((?:.|\n)+?)\[\/list\]/gmi, function (match, p1, offset, string) {return p1.replace(/\[\*\]/gmi, '1. ');})
-    .replace(/(\n)\[\*\]/gmi, '$1* ') //lists; replcae lists with + unordered lists.
-    .replace(/\[\/*list\]/gmi, '')
+    //bold; replace [b] $1 [/b] with ** $1 **
+    .replace(/\[b\]((?:.|\n)+?)\[\/b\]/gmi, '**$1**')
+    //underline to italic; replace [u] $1 [/u] with * $1 *
+    .replace(/\[\u\]((?:.|\n)+?)\[\/\u\]/gmi, '*$1*')
+    //italic; replace [i] $1 [/i] with * $1 *
+    .replace(/\[\i\]((?:.|\n)+?)\[\/\i\]/gmi, '*$1*')
+    //strikethrough; replace [s] $1 [/s] with ~~ $1 ~~
+    .replace(/\[s\]((?:.|\n)+?)\[\/s\]/gmi, '~~$1~~')
+    //remove [color] tags
+    .replace(/\[color\=.+?\]((?:.|\n)+?)\[\/color\]/gmi, '$1')
+    //unordered list; remove [list][/list] and convert it's elements
+    .replace(/\[list\]((?:.|\n)+?)\[\/list\]/gmi,
+        function (match, p1, offset, string) {
+          //unordered list element; convert [li] $text [/li] to * $text
+          return p1.replace(/\[li\]((?:.|\n)+?)\[\/li\]/gmi, '* $1');
+    })
+    //ordered list; remove [list type=decimal][/list] and convert it's elements
+    .replace(/\[list type=decimal\]((?:.|\n)+?)\[\/list\]/gmi,
+        function (match, p1, offset, string) {
+          //ordered list element; convert [li] $text [/li] to 1. $text
+          return p1.replace(/\[li\]((?:.|\n)+?)\[\/li\]/gmi, '1. $1');
+    })
+    //img; replace [img] $link [/url] with ![]($link)
     .replace(/\[img\]((?:.|\n)+?)\[\/img\]/gmi,'![]($1)')
+    //url; replace [url=$link] $text [/url] with [$text]($link)
     .replace(/\[url=(.+?)\]((?:.|\n)+?)\[\/url\]/gmi,'[$2]($1)')
+    //inline code; replace [code] $text [/code] with `$text`
+    .replace(/\[quote\](.*?)\[\/quote\]/gmi, '> $1')
+    //quote; remove [quote][/quote] convert it's elements
+    .replace(/\[quote\]((?:.|\n)+?)\[\/quote\]/gmi,
+        function (match, p1, offset, string) {
+          //quote lines; convert $line to > $line
+          return p1.replace(/((?:.*)\n)/gmi, '> $1');
+    })
+    //inline code; replace [code] $text [/code] with `$text`
     .replace(/\[code\](.*?)\[\/code\]/gmi, '`$1`')
-    .replace(/\[code\]((?:.|\n)+?)\[\/code\]/gmi, function (match, p1, offset, string) {return p1.replace(/^/gmi, '    ');});
+    //code block; replace [code=$lang] $text [/code] with ```$lang $text ```
+    .replace(/\[code=(.+?)\]((?:.|\n)+?)\[\/code\]/gmi, '```$1$2```')
+    //code block; replace [code] $text [/code] with ``` $text ```
+    .replace(/\[code\]((?:.|\n)+?)\[\/code\]/gmi, '```$1```')
+    //Below elements were compared with em representation of H* tags
+    //size 24 element; convert to H1
+    .replace(/\[size=24pt\]((?:.|\n)+?)\[\/size\]/gmi,'# $1')
+    //size 18 element; convert to H2
+    .replace(/\[size=18pt\]((?:.|\n)+?)\[\/size\]/gmi,'## $1')
+    //size 14 element; convert to H3
+    .replace(/\[size=14pt\]((?:.|\n)+?)\[\/size\]/gmi,'### $1')
+    //size 12 element; convert to H4
+    .replace(/\[size=12pt\]((?:.|\n)+?)\[\/size\]/gmi,'#### $1')
+    //size 10 element; convert to H5
+    .replace(/\[size=10pt\]((?:.|\n)+?)\[\/size\]/gmi,'##### $1')
+    //size 8 element; convert to H6
+    .replace(/\[size=8pt\]((?:.|\n)+?)\[\/size\]/gmi,'###### $1')
+    //horizontal line; replace [hr] with ---
+    .replace(/\[hr\]/i, '---');
 
-  //post processing for tf2toolbox BBCode
-  if(left_value.search(/TF2Toolbox/gmi) != -1) {
+  if (html_check) {
     left_value = left_value
-    .replace('/bbcode_lookup.php))', '/bbcode_lookup.php) and converted to /r/tf2trade ready Markdown by Dum\'s [converter](http://jondum.github.com/BBCode-To-Markdown-Converter/)).') //add a linkback
-    .replace(/\*\*.+?\*\*[\s]+?None[\s]{2}/gmi, ''); //remove empty sections
+    //table; replace [table] $text [/table] with html <table>$text</table>
+    .replace(/\[table\]((?:.|\n)+?)\[\/table\]/gmi, '<table>$1</table>')
+    //tr; replace [tr] $text [/tr] with html <tr>$text</tr>
+    .replace(/\[tr\]((?:.|\n)+?)\[\/tr\]/gmi, '<tr>$1</tr>')
+    //th; replace [th] $text [/th] with html <th>$text</th>
+    .replace(/\[th\]((?:.|\n)+?)\[\/th\]/gmi, '<th>$1</th>')
+    //td; replace [td] $text [/td] with html <td>$text</td>
+    .replace(/\[td\]((?:.|\n)+?)\[\/td\]/gmi, '<td>$1</td>')
 
   }
 
